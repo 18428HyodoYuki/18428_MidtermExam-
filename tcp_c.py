5695bca5b60256533c9fb5ab26c9bac746327001
18428HyodoYuki <18428@toba-cmt.ac.jp>
2021-11-17 15:12:23 +0900

    PEP８に従って変数の名前を変更しました

--- tcp_c.py
+++ tcp_c.py
@@ -3,5 +3,5 @@ import socket
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as SOCK:
     SOCK.connect(('127.0.0.1', 50007))
     SOCK.sendall(b'hello server')
-    Data = SOCK.recv(1024)
-    print(repr(Data))
+    data = SOCK.recv(1024)
+    print(repr(data))
